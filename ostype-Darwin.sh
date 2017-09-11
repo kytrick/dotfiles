5c38c144ae26d2bc9a427500dea6347b34c5869b
Kayla Smith <kytrick@gmail.com>
2017-09-10 18:01:24 -0700

    added brew update

--- ostype-Darwin.sh
+++ ostype-Darwin.sh
@@ -7,6 +7,7 @@ if ! which brew > /dev/null; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew update
 # Run Brewfile
 brew bundle # searches for a file called Brewfile in cwd
 
